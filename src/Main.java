228131f33c8a25bcd6eccf46f3e98fbae3e44e1b
zl8244 <zl8244@g.rit.edu>
2024-02-08 14:45:59 -0500

    Readded dice sides read from CLI

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         Character MC = new Character();
-        MC.characterAction("run", 6);
+        int numSides = parseArgs(args, 6);
+        MC.characterAction("run", numSides);
     }
 }
